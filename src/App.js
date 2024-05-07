25ca06eb19845a0cbf3e0382daf5b9d065a9bb14
winxbbbbb <panyasap.s65@rsu.ac.th>
2024-05-08 02:38:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   }
 
   const initLine = () => {
-    liff.init({ liffId: '1655665373-YAopzeO6' }, () => {
+    liff.init({ liffId: '2004847315-VeowYZqK' }, () => {
       if (liff.isLoggedIn()) {
         runApp();
       } else {
